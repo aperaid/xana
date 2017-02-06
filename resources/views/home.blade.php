871ab4dacbd6ed26b3692f7b825b954e58377f2b
Granth Chia <granth.gc@gmail.com>
2017-02-06 21:16:43 +0700

    dashboard v0.1
    asd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,100 @@
 @extends('layouts.xana.layout')
 @section('title')
-	Home
+	All Project
 @stop
 
 @section('content')
+
+<div class="row">
+  <div class="col-md-12">
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">General Reports</h3>
+
+        </div><!-- /.box-header -->
+      <div class="box-body">
+              <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>150</h3>
+
+              <p>Jatuh Tempo</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-shopping-cart"></i>
+            </div>
+            <a href="invoice" class="small-box-footer">
+              More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>53<sup style="font-size: 20px"></sup></h3>
+
+              <p>Stock</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="inventory/viewinventory" class="small-box-footer">
+              More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>44</h3>
+
+              <p>Surat Jalan</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-bus"></i>
+            </div>
+            <a href="sjkirim" class="small-box-footer">
+              More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>65</h3>
+
+              <p>Extend Sewa</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-share-square-o"></i>
+            </div>
+            <a href="transaksi" class="small-box-footer">
+              More info <i class="fa fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+                <!-- /.row -->
+      </div>
+            <!-- ./box-body -->
+        <div class="box-footer">
+        
+        </div>
+            <!-- /.box-footer -->
+    </div><!-- /.box -->
+  </div><!-- /.col -->
+</div> <!-- Row -->
+@stop
+
+@section('script')
 
 @stop
